dfc917027c157dd23999bce07a2a13951790ab49
mdwiastika <marceldwias@gmail.com>
2023-03-05 18:49:04 +0700

    add BlogDetail page

--- src/App.js
+++ src/App.js
@@ -9,17 +9,23 @@ import Footer from './partials/Footer';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
+import BlogDetail from './pages/BlogDetail';
+import NotFound from './pages/NotFound';
 function App() {
   return (
     <div className="text-dark">
       <Header></Header>
       <Routes>
         <Route index element={<Home title="Halaman Home" />}></Route>
-        <Route path="blog" element={<Blog title="Halaman Blog" />}></Route>
+        <Route path="blog">
+          <Route index element={<Blog title="Halaman Blog" />}></Route>
+          <Route path=":id_blog" element={<BlogDetail />}></Route>
+        </Route>
         <Route path="about" element={<About title="Halaman About" />}></Route>
         <Route path="login" element={<Login title="Form Login" />}></Route>
         <Route path="register" element={<Register title="Form Register" />}></Route>
         <Route path="dashboard" element={<Dashboard />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
       <Footer></Footer>
     </div>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+import {Container} from 'react-bootstrap';
+const NotFound = () => {
+  return <Container>Halaman Tak Ada</Container>;
+};
+export default NotFound;
