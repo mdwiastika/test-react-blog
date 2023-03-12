dff4d8c068b28ac45ba5dd7d9a45df2eca16400a
mdwiastika <marceldwias@gmail.com>
2023-03-12 15:55:41 +0700

    Insert Post

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,15 +4,42 @@ import Footer from './../partials/Footer';
 import {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 const Dashboard = () => {
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [content, setContent] = useState('');
   const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const form_blog = document.getElementById('form-blog');
+  const postBlog = async (event) => {
+    event.preventDefault();
+    const form_data = new FormData(form_blog);
+    const fetching_data = await fetch('http://127.0.0.1:8000/api/post', {
+      method: 'POST',
+      headers: {
+        Authorization: localStorage.token,
+      },
+      body: form_data,
+    });
+    const json_data = await fetching_data.json();
+    if (json_data.data) {
+      setTitle('');
+      setImage('');
+      setContent('');
+      getDataBlog();
+    }
+  };
+  const handleTextArea = (e) => {
+    setContent(e.target.value);
+  };
+  const getDataBlog = async () => {
+    const fetching_data = await fetch('http://127.0.0.1:8000/api/post', {
+      method: 'GET',
+    }).catch((err) => console.log({message: err}));
+    const json_data = await fetching_data.json();
+    setBlogs(json_data.data);
+    setLoading(false);
+  };
   useEffect(() => {
-    const getDataBlog = async () => {
-      const fetching_data = await fetch('http://127.0.0.1:8000/api/post', {
-        method: 'GET',
-      }).catch((err) => console.log({message: err}));
-      const json_data = await fetching_data.json();
-      setBlogs(json_data.data);
-    };
     getDataBlog();
   }, []);
   return (
@@ -24,22 +51,24 @@ const Dashboard = () => {
             <Card>
               <Card.Header>Tambah Post</Card.Header>
               <Card.Body>
-                <Form>
+                <Form onSubmit={postBlog} id="form-blog">
                   <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" name="title" placeholder="Enter title" />
+                    <Form.Control type="text" value={title} onInput={(e) => setTitle(e.target.value)} name="title" placeholder="Enter title" />
                   </Form.Group>
                   <Form.Group controlId="formFileSm" className="mb-3">
                     <Form.Label>Select image post</Form.Label>
-                    <Form.Control type="file" name="image" size="sm" />
+                    <Form.Control type="file" name="image" size="sm" value={image} onInput={(e) => setImage(e.target.value)} />
                   </Form.Group>
                   <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Content</Form.Label>
-                    <Form.Control as="textarea" rows={3} name="body" />
+                    <Form.Control as="textarea" rows={3} name="body" value={content} onChange={handleTextArea}>
+                      {content}
+                    </Form.Control>
                     <input type="hidden" name="user_id" />
                   </Form.Group>
                   <Button variant="primary" type="submit">
-                    Login
+                    Tambah
                   </Button>
                 </Form>
               </Card.Body>
@@ -56,23 +85,27 @@ const Dashboard = () => {
                 </tr>
               </thead>
               <tbody>
-                <>
-                  {blogs.map((blog, key) => {
-                    return (
-                      <tr>
-                        <td>{key + 1}</td>
-                        <td>{blog.title}</td>
-                        <td>
-                          <img src={'http://127.0.0.1:8000/storage/' + blog.image} width="200" style={{objectFit: 'cover'}} height="150" alt="" />
-                        </td>
-                        <td>
-                          <Link className="btn btn-warning text-white me-2">Edit</Link>
-                          <Link className="btn btn-danger text-white">Hapus</Link>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </>
+                {loading ? (
+                  'Masih loading...'
+                ) : (
+                  <>
+                    {blogs.map((blog, key) => {
+                      return (
+                        <tr>
+                          <td>{key + 1}</td>
+                          <td>{blog.title}</td>
+                          <td>
+                            <img src={'http://127.0.0.1:8000/storage/' + blog.image} width="200" style={{objectFit: 'cover'}} height="150" alt="" />
+                          </td>
+                          <td>
+                            <Link className="btn btn-warning text-white me-2">Edit</Link>
+                            <Link className="btn btn-danger text-white">Hapus</Link>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </>
+                )}
               </tbody>
             </Table>
           </Col>
--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,7 +1,9 @@
+import {useEffect, useState} from 'react';
 import {Navbar, Container, Nav, Form, Button, NavDropdown} from 'react-bootstrap';
 import {Link, NavLink, useNavigate} from 'react-router-dom';
 const Header = () => {
   const navigate = useNavigate();
+  const [user, setUser] = useState([]);
   const postLogout = async () => {
     const fetchingData = await fetch('http://127.0.0.1:8000/api/logout', {
       method: 'POST',
@@ -20,6 +22,20 @@ const Header = () => {
     console.log('berhasil logout');
     navigate('/login');
   };
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const fetching_data = await fetch('http://127.0.0.1:8000/api/user', {
+        method: 'GET',
+        headers: {
+          Authorization: localStorage.token,
+          'Content-Type': 'application/json',
+        },
+      });
+      const json_data = await fetching_data.json();
+      setUser(json_data);
+    };
+    getUserInfo();
+  }, []);
   return (
     <Navbar bg="white" style={{borderBottom: '2px solid black', position: 'sticky', top: '0', zIndex: '99'}} className="p-3" expand="lg">
       <Container>
@@ -42,8 +58,15 @@ const Header = () => {
               {localStorage.token ? (
                 <>
                   <NavDropdown.Item>
-                    <Link to={'/dashboard'}>Dashboard</Link>
+                    {user.name} ({user.role})
                   </NavDropdown.Item>
+                  {user.role === 'admin' ? (
+                    <NavDropdown.Item>
+                      <Link to={'/dashboard'}>Dashboard</Link>
+                    </NavDropdown.Item>
+                  ) : (
+                    ''
+                  )}
                   <Form onSubmit={formLogout} method="POST">
                     <button type="submit" style={{marginLeft: '10px', backgroundColor: 'transparent', border: 'none'}} className="text-danger">
                       Logout
