be551e7b3aa63bb0286a9cb6c0a7b11b94fdcd87
mdwiastika <marceldwias@gmail.com>
2023-03-12 19:50:57 +0700

    add feature edit + delete post for admin

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -3,6 +3,7 @@ import {Button, Card, Col, Container, Row} from 'react-bootstrap';
 import {Link, useNavigate, useParams} from 'react-router-dom';
 import Header from './../partials/Header';
 import Footer from './../partials/Footer';
+import './../assets/css/star.css';
 const BlogDetail = () => {
   const navigate = useNavigate();
   const [blog, setBlog] = useState({});
@@ -30,6 +31,31 @@ const BlogDetail = () => {
               <h2 className="text-center">{blog.title}</h2>
               <img src={'http://127.0.0.1:8000/storage/' + blog.image} className="img-fluid rounded-2" alt="gambar-blog" />
               <p>{blog.body}</p>
+              <div class="container d-flex justify-content-center mt-200">
+                <div class="stars">
+                  <form action="">
+                    <input class="star star-5" id="star-5" type="radio" name="star" />
+
+                    <label class="star star-5" for="star-5"></label>
+
+                    <input class="star star-4" id="star-4" type="radio" name="star" />
+
+                    <label class="star star-4" for="star-4"></label>
+
+                    <input class="star star-3" id="star-3" type="radio" name="star" />
+
+                    <label class="star star-3" for="star-3"></label>
+
+                    <input class="star star-2" id="star-2" type="radio" name="star" />
+
+                    <label class="star star-2" for="star-2"></label>
+
+                    <input class="star star-1" id="star-1" type="radio" name="star" />
+
+                    <label class="star star-1" for="star-1"></label>
+                  </form>
+                </div>
+              </div>
               <Link to={'/blog'}>
                 <Button variant="danger">Kembali</Button>
               </Link>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,14 +2,17 @@ import {Button, Card, Col, Container, Form, Row, Table} from 'react-bootstrap';
 import Header from './../partials/Header';
 import Footer from './../partials/Footer';
 import {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
+import {json, Link} from 'react-router-dom';
 const Dashboard = () => {
+  const [formPost, setFormPost] = useState('');
   const [title, setTitle] = useState('');
   const [image, setImage] = useState('');
   const [content, setContent] = useState('');
+  const [idBlog, setIdBlog] = useState('');
   const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
   const form_blog = document.getElementById('form-blog');
+  const container_form = document.getElementById('container-form');
   const postBlog = async (event) => {
     event.preventDefault();
     const form_data = new FormData(form_blog);
@@ -25,6 +28,119 @@ const Dashboard = () => {
       setTitle('');
       setImage('');
       setContent('');
+      getDataBlog();
+    }
+  };
+  const postFormHandler = () => {
+    if (formPost === 'create' || formPost === '') {
+      return (
+        <Card>
+          <Card.Header>Tambah Post</Card.Header>
+          <Card.Body>
+            <Form onSubmit={postBlog} id="form-blog">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" value={title} onInput={(e) => setTitle(e.target.value)} name="title" placeholder="Enter title" />
+              </Form.Group>
+              <Form.Group controlId="formFileSm" className="mb-3">
+                <Form.Label>Select image post</Form.Label>
+                <Form.Control type="file" name="image" size="sm" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Content</Form.Label>
+                <Form.Control as="textarea" rows={3} name="body" value={content} onChange={handleTextArea}>
+                  {content}
+                </Form.Control>
+                <input type="hidden" name="user_id" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Tambah
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      );
+    } else if (formPost === 'edit') {
+      return (
+        <Card>
+          <Card.Header>Edit Post</Card.Header>
+          <Card.Body>
+            <Form onSubmit={postUpdateHandler} id="form-blog">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Title</Form.Label>
+                <Form.Control type="text" value={title} onInput={(e) => setTitle(e.target.value)} name="title" placeholder="Enter title" />
+              </Form.Group>
+              <Form.Group controlId="formFileSm" className="mb-3">
+                <Form.Label>Select image post</Form.Label>
+                <Form.Control type="file" name="image" size="sm" />
+                <img src={'http://127.0.0.1:8000/storage/' + image} width="200" className={'mt-3'} style={{objectFit: 'cover'}} height="150" alt="" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Content</Form.Label>
+                <Form.Control as="textarea" rows={3} name="body" value={content} onChange={handleTextArea}>
+                  {content}
+                </Form.Control>
+                <input type="hidden" name="user_id" />
+              </Form.Group>
+              <Button variant="warning text-white" type="submit">
+                Update
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      );
+    } else {
+      return '';
+    }
+  };
+  const postCreateHandler = () => {
+    setFormPost('create');
+    setTitle('');
+    setImage('');
+    setContent('');
+    postFormHandler();
+  };
+  const postEditHandler = async (id) => {
+    console.log('halo');
+    const fetching_data = await fetch(`http://127.0.0.1:8000/api/post/${id}`, {
+      method: 'GET',
+    });
+    const json_data = await fetching_data.json();
+    setFormPost('edit');
+    setTitle(json_data.data.title);
+    setImage(json_data.data.image);
+    setContent(json_data.data.body);
+    setIdBlog(json_data.data.id);
+    console.log(json_data);
+    postFormHandler();
+  };
+  const postUpdateHandler = async (event) => {
+    event.preventDefault();
+    const form_data = new FormData(form_blog);
+    console.log(idBlog);
+    console.log(form_blog);
+    const fetching_data = await fetch(`http://127.0.0.1:8000/api/post/${idBlog}?_method=PUT`, {
+      method: 'POST',
+      headers: {
+        Authorization: localStorage.token,
+      },
+      body: form_data,
+    }).catch((err) => console.log({message: err}));
+    const json_data = await fetching_data.json().catch((err) => console.log({message: err}));
+    if (json_data.data) {
+      getDataBlog();
+      postCreateHandler();
+    }
+  };
+  const postRemoveHandler = async (id) => {
+    const fetching_data = await fetch(`http://127.0.0.1:8000/api/post/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: localStorage.token,
+      },
+    });
+    const json_data = await fetching_data.json();
+    if (json_data.data) {
       getDataBlog();
     }
   };
@@ -47,32 +163,11 @@ const Dashboard = () => {
       <Header></Header>
       <Container className="min-vh-100">
         <Row className="d-flex justify-content-center mt-4 g-2">
-          <Col md={12} lg={5}>
-            <Card>
-              <Card.Header>Tambah Post</Card.Header>
-              <Card.Body>
-                <Form onSubmit={postBlog} id="form-blog">
-                  <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Title</Form.Label>
-                    <Form.Control type="text" value={title} onInput={(e) => setTitle(e.target.value)} name="title" placeholder="Enter title" />
-                  </Form.Group>
-                  <Form.Group controlId="formFileSm" className="mb-3">
-                    <Form.Label>Select image post</Form.Label>
-                    <Form.Control type="file" name="image" size="sm" value={image} onInput={(e) => setImage(e.target.value)} />
-                  </Form.Group>
-                  <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Content</Form.Label>
-                    <Form.Control as="textarea" rows={3} name="body" value={content} onChange={handleTextArea}>
-                      {content}
-                    </Form.Control>
-                    <input type="hidden" name="user_id" />
-                  </Form.Group>
-                  <Button variant="primary" type="submit">
-                    Tambah
-                  </Button>
-                </Form>
-              </Card.Body>
-            </Card>
+          <Button variant="success" onClick={postCreateHandler.bind(this)}>
+            Tambah Data
+          </Button>
+          <Col md={12} lg={5} id="container-form">
+            {postFormHandler()}
           </Col>
           <Col md={12} lg={7}>
             <Table striped bordered hover>
@@ -98,8 +193,12 @@ const Dashboard = () => {
                             <img src={'http://127.0.0.1:8000/storage/' + blog.image} width="200" style={{objectFit: 'cover'}} height="150" alt="" />
                           </td>
                           <td>
-                            <Link className="btn btn-warning text-white me-2">Edit</Link>
-                            <Link className="btn btn-danger text-white">Hapus</Link>
+                            <Link className="btn btn-warning text-white me-2" onClick={postEditHandler.bind(this, blog.id)}>
+                              Edit
+                            </Link>
+                            <Link className="btn btn-danger text-white" onClick={postRemoveHandler.bind(this, blog.id)}>
+                              Hapus
+                            </Link>
                           </td>
                         </tr>
                       );
